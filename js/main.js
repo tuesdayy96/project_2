add7cec0f2fe8590a588d701b9cfdd4fb8ba4264
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-05-18 17:44:56 +0900

    project2.ver1.5
    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-    $('.menu')
-})
+var tap = document.getElementById('navtap');
+
+function openbtn(){
+    tap.style.width = '90%';
+}
+function closebtn(){
+    tap.style.width = '0';
+}
