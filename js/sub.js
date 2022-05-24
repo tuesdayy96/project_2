28e3e0b7b06520148c3bf0c002d5387873d4e6c3
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-05-24 15:29:30 +0900

    project2.ver2.2

--- js/sub.js
+++ js/sub.js
@@ -58,3 +58,40 @@ r_delux.addEventListener('mouseleave',r_hoverOut);
 r_suite.addEventListener('mouseleave',r_hoverOut);
 }
 room();
+
+// 슬라이드 스크립트
+
+var banner_left = 0;
+var imgCnt = 0;
+var first = 1;
+var last;
+var interval;
+$(document).ready(function(){
+    $('.slides a').each(function(){
+        $(this).css('left',banner_left);
+        banner_left += $(this).width()+5;
+        $(this).attr('id','content'+(++imgCnt));
+    });
+    last = imgCnt;
+    rollingStart();
+});
+function rollingStart(){
+    interval = setInterval(function(){
+        $('.slides a').each(function(){
+            $(this).css('left',$(this).position().left-1);
+        });
+        var first_content = $('#content' + first);
+        var last_content = $('#content' + last);
+        if(first_content.position().left < '-'+$(first_content).width()){
+            first_content.css('left', last_content.position().left+last_content.width()+5);
+            first++;
+            last++;
+            if(last>imgCnt){
+                last = 1;
+            }
+            if(first>imgCnt){
+                first = 1;
+            }
+        }
+    },10);
+}
